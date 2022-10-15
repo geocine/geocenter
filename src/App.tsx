0bcafb5a3e4ba535d558b35ca782a9017a14d54e
Aivan Monceller <aivan@monceller.com>
2022-10-16 00:28:02 +0800

    feat(sidebar): add proper sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -48,32 +48,58 @@ function App() {
 
   return (
     <div className="App flex h-screen bg-gray-50 font-roboto">
-      <div className="flex flex-col flex-shrink-0 w-64">
-        <div className="flex items-center h-16 flex-shrink-0 px-4 bg-indigo-600">
-          <span className="text-xl font-semibold tracking-wide text-white">
-            Geocenter
-          </span>
-        </div>
-        <div className="flex-1 overflow-y-auto overflow-x-hidden bg-white">
-          <nav className="flex flex-col py-4 space-y-1">
-            <a
-              href="#"
-              className="flex items-center px-6 py-2 space-x-2 text-indigo-600 bg-indigo-100"
+      <div className="flex flex-col items-center w-16 h-full overflow-hidden text-gray-400 bg-gray-900">
+        <a className="flex items-center justify-center mt-3" href="#">
+          <svg
+            className="w-8 h-8 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z"></path>
+          </svg>
+        </a>
+        <div className="flex flex-col items-center mt-3 border-t border-gray-700">
+          <a
+            className="flex items-center justify-center w-12 h-12 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
+            href="#"
+          >
+            <svg
+              className="w-6 h-6 stroke-current"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <span className="text-sm font-medium tracking-wide text-indigo-600">
-                Home
-              </span>
-            </a>
-            <a
-              href="#"
-              className="flex items-center px-6 py-2 space-x-2 text-gray-600 hover:bg-gray-100 hover:text-gray-900"
-            >
-              <span className="text-sm font-medium tracking-wide text-gray-600">
-                Misc
-              </span>
-            </a>
-          </nav>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+              ></path>
+            </svg>
+          </a>
+
         </div>
+        <a
+          className="flex items-center justify-center w-16 h-16 mt-auto bg-gray-800 hover:bg-gray-700 hover:text-gray-300"
+          href="#"
+        >
+          <svg
+            className="w-6 h-6 stroke-current"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+        </a>
       </div>
       <div className="flex-1 overflow-hidden">
         <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none">
